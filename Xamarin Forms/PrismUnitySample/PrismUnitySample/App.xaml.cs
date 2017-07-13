6091865c490038e969a94a88735efa6dea1497f4
Charlin Agramonte <char0394@gmail.com>
2017-07-13 14:50:05 -0400

    Add Prism Unity sample

---
+++ Xamarin Forms/PrismUnitySample/PrismUnitySample/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Unity;
+using PrismUnitySample.ViewModels;
+using PrismUnitySample.Views;
+using Xamarin.Forms;
+
+namespace PrismUnitySample
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync(new System.Uri("http://www.MyTestApp/LoginPage", System.UriKind.Absolute));
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<LoginPage, LoginPageViewModel>();
+            Container.RegisterTypeForNavigation<HomePage,HomePageViewModel>();
+        }
+    }
+}
---
+++ Xamarin Forms/PrismUnitySample/PrismUnitySample/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace PrismUnitySample.ViewModels
+{
+    public class HomePageViewModel : BindableBase,INavigatedAware
+    {
+		private string _userName;
+		public string UserName
+		{
+			get { return _userName; }
+			set { SetProperty(ref _userName, value); }
+		}
+
+        INavigationService _navigationService;
+        public HomePageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        void INavigatedAware.OnNavigatedFrom(NavigationParameters parameters)
+        {
+            
+        }
+
+        void INavigatedAware.OnNavigatedTo(NavigationParameters parameters)
+        {
+            if(parameters.ContainsKey("UserName")){
+                UserName = parameters.GetValue<string>("UserName");
+            }
+        }
+    }
+}
