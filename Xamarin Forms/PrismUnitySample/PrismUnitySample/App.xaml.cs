f653df7d8619e63fadaad9d1754d4d95e12ac93c
Charlin Agramonte <char0394@gmail.com>
2017-08-16 09:12:38 -0400

    Add passport form

--- Xamarin Forms/PrismUnitySample/PrismUnitySample/App.xaml.cs
+++ Xamarin Forms/PrismUnitySample/PrismUnitySample/App.xaml.cs
@@ -15,15 +15,26 @@ namespace PrismUnitySample
         {
             InitializeComponent();
 
-            NavigationService.NavigateAsync(new System.Uri("http://www.MyTestApp/CustomNavigationPage/LoginPage", System.UriKind.Absolute));
 
-        }
+			//Descomment this list to Test Prism Tabbed Page 
+			NavigationService.NavigateAsync(new System.Uri("/CustomTabbedPage/NavigationPage/Test1Page", System.UriKind.Absolute));
+
+			//Descomment this list to Test Prism Master DetailPage 
+
+			//Descomment this list to Test Prism general concepts as Custom NavigationPage/Modules/DelegateCommands/Services 
+			//NavigationService.NavigateAsync(new System.Uri("http://www.MyTestApp/CustomNavigationPage/LoginPage", System.UriKind.Absolute));
+
+		}
 
         protected override void RegisterTypes()
         {
+			Container.RegisterTypeForNavigation<CustomTabbedPage>();
             Container.RegisterTypeForNavigation<CustomNavigationPage>();
             Container.RegisterTypeForNavigation<LoginPage, LoginPageViewModel>();
             Container.RegisterTypeForNavigation<HomePage, HomePageViewModel>();
+
+            Container.RegisterTypeForNavigation<Test1Page>();
+            Container.RegisterTypeForNavigation<Test2Page>();
         }
 
 		protected override void ConfigureModuleCatalog()
---
+++ Xamarin Forms/PrismUnitySample/PrismUnitySample/Views/CustomTabbedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PrismUnitySample.Views
+{
+    public partial class CustomTabbedPage : TabbedPage
+    {
+        public CustomTabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
